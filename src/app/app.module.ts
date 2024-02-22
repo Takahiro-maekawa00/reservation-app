2f5cc0a081fb5a1ab260937ed86cc128386264af
takahiro-maekawa <takahiro.maekawa@sony.com>
2024-02-22 02:08:42 +0000

    Authフロントエンド部分作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { NavbarComponent } from './common/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule,
+    AuthModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const authRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(authRoutes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
